92891fbf389714f34a324eeea26639074775a3df
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-26 19:07:17 +0200

    Download directory
    A download directory will be created that will contain files
    downloaded from the internet.

--- android-experiment/src/main/scripts/install-android.sh
+++ android-experiment/src/main/scripts/install-android.sh
@@ -1,3 +1,11 @@
 #! /usr/bin/env bash
 
+DOWNLOAD_DIRECTORY=sdk
+
 echo "Installing Android SDK."
+
+if [ ! -d $DOWNLOAD_DIRECTORY ]
+then
+    echo "Creating directory" $DOWNLOAD_DIRECTORY
+    mkdir $DOWNLOAD_DIRECTORY
+fi
